6a74951c58c459fc346607432b82e9df60273edf
Daniel Kovacs <info@kovix.info>
2021-11-18 19:22:06 +0100

    solved

--- solutions/app1.js
+++ solutions/app1.js
@@ -1,7 +1,11 @@
+const converterPromise = (arr) => new Promise((resolve, reject) => {
+    if (arr.every((item) => typeof item === 'string')) {
+        resolve(arr.map((item) => item.toLocaleUpperCase()));
+    } else {
+        reject('Error: Not all elements are string type!');
+    }
+});
 
 
 
-
-
-
-// export default converterPromise;
+export default converterPromise;
